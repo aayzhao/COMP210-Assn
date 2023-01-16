01a21838141a7fd1f262b529a64b61f8f0130dcc
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-01-16 14:46:47 -0500

    part3 update4 1/16/2023

--- src/assn01/Part3.java
+++ src/assn01/Part3.java
@@ -21,6 +21,12 @@ public class Part3
     public static void method3()
     {
         int[] a3 = new int[]{'a','z'};
+        /*
+        *declaring a3 as charArray would
+        *prevent the print statement from giving numbers, so
+        *a3 is declared as an int array for the ascii
+        *numbers to be printed.
+         */
         System.out.println(a3[0]+" "+a3[1]);
     }
 }
