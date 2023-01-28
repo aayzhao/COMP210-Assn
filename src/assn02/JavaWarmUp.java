efc01a719f3e05c54c9efd46f0bf5b8af6736617
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-01-28 00:17:24 -0500

    1/27/2023 assn02 update 3

--- src/assn02/JavaWarmUp.java
+++ src/assn02/JavaWarmUp.java
@@ -11,11 +11,17 @@ public class JavaWarmUp {
         //creating  new scanner object
         Scanner scan = new Scanner(System.in);
 
+        //query for batch numbers
         System.out.println("Enter entry count:");
         itemCount = scan.nextInt();
 
+        //create array to store ProductBatch objects
+        ProductBatch[] prodList = new ProductBatch[itemCount];
+
+        //query for and add ProductBatch to array
         for(int i = 0; i < itemCount; i++){
             System.out.println("Enter ProductBatch Batch #" + (i + 1));
+            prodList[i] = new ProductBatch(scan.nextLine().split(" "));
         }
 
     }
--- src/assn02/ProductBatch.java
+++ src/assn02/ProductBatch.java
@@ -23,13 +23,49 @@ public class ProductBatch {
     public ProductBatch(String date, String time, String category,
                         Double fee, int quantity,
                         Double aTime, Double cost){
-        _date =  date;
+        _date = date;
         _time = time;
         _category = category;
         _fee = fee;
         _quantity = quantity;
         _aTime = aTime;
         _cost = cost;
+    }
+
+    //String[] constructor
+    //Assumes a String[] of length 7 is given, with date, time, etc in order starting with
+    //date at index 0
+    public ProductBatch(String[] batch){
+        _date = batch[0];
+        _time = batch[1];
+        _category = batch[2];
+        _fee = Double.valueOf(batch[3]);
+        _quantity = Integer.parseInt(batch[4]);
+        _aTime = Double.valueOf(batch[5]);
+        _cost = Double.valueOf(batch[6]);
+    }
+
+    //accessor methods
+    public String get_date(){
+        return _date;
+    }
+    public String get_time(){
+        return _time;
+    }
+    public String get_category(){
+        return _category;
+    }
+    public Double get_fee(){
+        return _fee;
+    }
+    public int get_quantity(){
+        return _quantity;
+    }
+    public Double get_aTime(){
+        return _aTime;
+    }
+    public Double get_cost(){
+        return _cost;
     }
 
 
