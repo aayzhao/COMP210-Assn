ce3f5442d9c9f9d6849addfa959409313fac06c2
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-01-28 01:26:41 -0500

    1/27/2023 assn02 update 6

--- src/assn02/JavaWarmUp.java
+++ src/assn02/JavaWarmUp.java
@@ -11,20 +11,68 @@ public class JavaWarmUp {
         Scanner scan = new Scanner(System.in);
 
         //query for batch numbers
-        //System.out.println("Enter entry count:");
         itemCount = Integer.parseInt(scan.nextLine());
 
-        //create array to store ProductBatch objects
+        //create array and allocate memory to store ProductBatch objects
         ProductBatch[] prodList = new ProductBatch[itemCount];
 
         //query for and add ProductBatch to array
         for(int i = 0; i < itemCount; i++){
-            //System.out.println("Enter ProductBatch Batch #" + (i + 1));
             prodList[i] = new ProductBatch(scan.nextLine().split(" "));
         }
 
         printList(prodList);
+        System.out.println();
+        printHighest(prodList);
+        printLowest(prodList);
 
+    }
+
+
+    //helper methods
+    //find/calculate statistics based on given list
+
+    //finds index of the ProductBatch with the highest price/fee
+    public static int getMaxPriceIndex(ProductBatch[] list) {
+        int index = list.length - 1;
+        for (int i = list.length - 2; i >= 0; i--) {
+            if (list[index].get_fee() < list[i].get_fee()) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    //finds index of the ProductBatch with the lowest price/fee
+    public static int getMinPriceIndex(ProductBatch[] list) {
+        int index = list.length - 1;
+        for (int i = list.length - 2; i >= 0; i--) {
+            if (list[index].get_fee() > list[i].get_fee()) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
+    //void printing methods
+
+    //void method for printing the highest assembly fee (that comes latest in the list)
+    public static void printHighest(ProductBatch[] list){
+        int index = getMaxPriceIndex(list);
+        System.out.println("Highest per unit assembling fee:");
+        System.out.println(" When: " + list[index].get_date() + " " + list[index].get_time());
+        System.out.println(" Category: " + list[index].get_category());
+        System.out.println(" Price: " + list[index].get_fee());
+    }
+
+    //void method for printing the lowest assembly fee (that comes latest in the list)
+    public static void printLowest(ProductBatch[] list){
+        int index = getMinPriceIndex(list);
+        System.out.println("Lowest per unit assembling fee:");
+        System.out.println(" When: " + list[index].get_date() + " " + list[index].get_time());
+        System.out.println(" Category: " + list[index].get_category());
+        System.out.println(" Price: " + list[index].get_fee());
     }
 
     //void method to print out the list of product batches. Testing purposes
