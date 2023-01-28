ad1fc861903c225809d07b7eb5318d99900a2dee
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-01-28 01:04:16 -0500

    1/27/2023 assn02 update 5

--- src/assn02/JavaWarmUp.java
+++ src/assn02/JavaWarmUp.java
@@ -11,15 +11,15 @@ public class JavaWarmUp {
         Scanner scan = new Scanner(System.in);
 
         //query for batch numbers
-        System.out.println("Enter entry count:");
-        itemCount = scan.nextInt();
+        //System.out.println("Enter entry count:");
+        itemCount = Integer.parseInt(scan.nextLine());
 
         //create array to store ProductBatch objects
         ProductBatch[] prodList = new ProductBatch[itemCount];
 
         //query for and add ProductBatch to array
         for(int i = 0; i < itemCount; i++){
-            System.out.println("Enter ProductBatch Batch #" + (i + 1));
+            //System.out.println("Enter ProductBatch Batch #" + (i + 1));
             prodList[i] = new ProductBatch(scan.nextLine().split(" "));
         }
 
@@ -30,6 +30,7 @@ public class JavaWarmUp {
     //void method to print out the list of product batches. Testing purposes
     public static void printList(ProductBatch[] list){
         for(int i = 0; i < list.length; i++){
+            System.out.println("Index: " + i);
             System.out.println("Date: " + list[i].get_date());
             System.out.println("Time: " + list[i].get_time());
             System.out.println("Category: " + list[i].get_category());
