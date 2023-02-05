ef5acb2de4f40d42431c3515c9163c03cb5399a3
Aaroz <aaron.y.zhao1@gmail.com>
2023-02-05 13:44:27 -0500

    2/5/2023 Practice for midterm files

--- src/selfPractice/linkedListPractice.java
+++ src/selfPractice/linkedListPractice.java
@@ -1,7 +1,25 @@
 package selfPractice;
 
+import java.util.LinkedList;
+
 public class linkedListPractice {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("Hello");
+        ll.add("world");
+        ll.add(1, "_");
+
+        System.out.println("Initial LinkedList " + ll);
+
+        // Function call
+        ll.remove(1);
+
+        System.out.println("After the Index Removal " + ll);
+
+        ll.remove("Hello");
+
+        System.out.println("After the Object Removal " + ll);
 
     }
 
