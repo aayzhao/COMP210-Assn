153ad893c46c4c4fb0b14bbe4422b4861d4f0b7d
Aaroz <aaron.y.zhao1@gmail.com>
2023-02-17 14:52:28 -0500

    2/17/2023 Assn03 task 1,2,3,4 complete

--- src/assn03/test.java
+++ src/assn03/test.java
@@ -4,17 +4,19 @@ public class test {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
-        //list.add(1);
+        list.add(1);
         list.add(2);
         list.add(3);
-        //list.add(3);
-        //list.add(3);
+        list.add(3);
+        list.add(3);
 
         LinkedList list2 = new LinkedList();
         list2.add(1);
         list2.add(2);
         list2.add(3);
+        list2.add(10);
 
+        /*
         System.out.println(list);
         //System.out.println(list2 + "\n");
 
@@ -26,7 +28,15 @@ public class test {
 
         //System.out.println(list.isEqual(list2));
 
-        //list.removeRepeats();
-        //System.out.print(list);
+        list.removeRepeats();
+        System.out.println(list);
+        System.out.println(list.getHead().getValue());
+        System.out.println(list.getTail().getValue());
+
+         */
+
+        System.out.println(list2);
+        list2.reverse();
+        System.out.println(list2);
     }
 }
