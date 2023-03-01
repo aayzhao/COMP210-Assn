36e626dfeca58e372b982140001552ec82707d83
Aaroz <aaron.y.zhao1@gmail.com>
2023-03-01 16:06:25 -0500

    3/1/2023 Code cleaning/formatting

--- src/assn04/Main.java
+++ src/assn04/Main.java
@@ -1,26 +1,19 @@
 package assn04;
 
-public class Main {
-  public static void main(String[] args) {
-    /*
-    This is a basic example of how to create a BST and add values to it.
-    You should add more examples and use this class to debug your code
-    */
+public class Main { public static void main(String[] args) {
     BST<Integer> bst = new NonEmptyBST<>(3);
-      bst = bst.insert(8);
-      bst = bst.insert(1);
-      bst = bst.insert(9);
-      bst = bst.insert(4);
-      bst = bst.insert(2);
-      bst = bst.insert(5);
-      bst = bst.insert(6);
+    bst = bst.insert(8);
+    bst = bst.insert(1);
+    bst = bst.insert(9);
+    bst = bst.insert(4);
+    bst = bst.insert(2);
+    bst = bst.insert(5);
+    bst = bst.insert(6);
 
+    //bst = bst.remove(3);
+    bst.printBreadthFirstTraversal();
 
-      //bst = bst.remove(3);
-      bst.printBreadthFirstTraversal();
-
-
-      System.out.println("Done");
+    System.out.println("Done");
   }
 
 }
